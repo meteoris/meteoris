e268be2857f939d8b33c10cbc70b2d328d6a1a25
radiegtya <radiegtya@yahoo.co.id>
2015-11-02 13:09:51 +0700

    user should be able to be hooked

--- packages/meteoris:user/lib/collections/User.js
+++ packages/meteoris:user/lib/collections/User.js
@@ -1,13 +1,1 @@
-//activate groundDB for users collection to work offline
-//GroundDB(Meteor.users);
-Meteor.users.allow({
-    insert: function(userId, doc) {
-        return Meteoris.Role.userIsInGroup("admin");
-    },
-    update: function(userId, doc) {
-        return Meteoris.Role.userIsInGroup("admin");
-    },
-    remove: function(userId, doc) {
-        return Meteoris.Role.userIsInGroup("admin");
-    },
-});
+//You can ovverride it on your collection
