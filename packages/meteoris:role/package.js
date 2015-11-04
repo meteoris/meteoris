9e0b242623963bc9d4579d2392074252b2d73de5
radiegtya <radiegtya@yahoo.co.id>
2015-11-04 12:34:57 +0700

    fixing role in english and adding user helper isExist for client access

--- packages/meteoris:role/package.js
+++ packages/meteoris:role/package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'meteoris:role',
-    version: '0.0.3',
+    version: '0.0.4',
     // Brief, one-line summary of the package.
     summary: 'Meteoris package for ACL',
     // URL to the Git repository containing the source code for this package.
