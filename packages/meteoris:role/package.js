62bf0ed4d9c02a3c23c9b428ff019f3b87611dad
radiegtya <radiegtya@yahoo.co.id>
2015-11-04 11:28:50 +0700

    fixing role

--- packages/meteoris:role/package.js
+++ packages/meteoris:role/package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'meteoris:role',
-    version: '0.0.2',
+    version: '0.0.3',
     // Brief, one-line summary of the package.
     summary: 'Meteoris package for ACL',
     // URL to the Git repository containing the source code for this package.
